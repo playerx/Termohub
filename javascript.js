e696764b5b13285f2293fca49724e087a3a888ac
kavtara1 <40422017+kavtara1@users.noreply.github.com>
2019-06-05 22:53:32 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function dropdown() {
+    var x = document.getElementById("drop_down_menu_content");
+   if (x.style.display==="none"){
+       x.style.display ="block";
+   }
+           
+    
+  }
