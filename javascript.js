7592aff495ebba44717ec2430d2ea6542ae052b3
kavtara <kavtara123>
2019-06-06 23:44:11 +0400

    dawd
    bla bla bla

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function dropdown() {
-    var x = document.getElementById("drop_down_menu_content");
-   if (x.style.display==="none"){
-       x.style.display ="block";
-   }
-           
-    
-  }
