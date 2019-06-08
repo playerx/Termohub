999383f2878f024f72a63eddb656934e28677d53
kavtara <kavtara123>
2019-06-09 00:51:12 +0400

    Some more stuff

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -6,3 +6,42 @@ function dropdown() {
            
     
   }
+
+  function hoverAddress() {
+      document.getElementById('Path_97').style.fill = '#6CB43F';
+
+  }
+  function undoHoverAddress() {
+    document.getElementById('Path_97').style.fill = "#00718e";
+  }
+
+  function hoverPhone() {
+      document.getElementById('Path_25').style.fill ='#6CB43F';
+  }
+
+  function undoHoverPhone() {
+    document.getElementById('Path_25').style.fill ='#00718e';
+  }
+
+  function hoverEmail() {
+    document.getElementById('Path_26').style.fill ='#6CB43F';
+    document.getElementById('Path_27').style.fill ='#6CB43F';
+}
+
+function unHoverEmail() {
+    document.getElementById('Path_26').style.fill ='#00718e';
+    document.getElementById('Path_27').style.fill ='#00718e';
+}
+
+
+function mediaHover() {
+    document.getElementById('Ellipse_1').style.fill= '#6CB43F';
+    document.getElementById('Path_64').style.fill = '#FFF';
+}
+
+function undoMediaHover() {
+    document.getElementById('Ellipse_1').style.fill= 'none';
+    document.getElementById('Path_64').style.fill = '#798296';
+}
+
+
